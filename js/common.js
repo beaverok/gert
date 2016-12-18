e5d06da9fb89366895f7e7daefe0abaca18a05ed
beaverok <askgroup77@gmail.com>
2016-12-18 20:34:34 +0500

    add adaptive

--- js/common.js
+++ js/common.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
    // mobile menu open-close
-    $('.page-header__menu-btn').click(function () {
+    $('.main-menu__open-btn').click(function () {
         $('.main-menu').slideToggle();
     });
-    $(document).click(function(event) {
+    /*$(document).click(function(event) {
         if ($(window).width() < 767 && $('.main-menu:visible')) {
             if ($(event.target).closest(".page-header").length) return;
             $('.main-menu').slideUp();
             event.stopPropagation();
         }
+    });*/
+
+    $('.top-menu__open-btn').click(function () {
+        $('.top-menu').slideToggle();
     });
 
 
@@ -20,4 +24,23 @@ $(document).ready(function () {
     $('.auction-item__counter').timeTo(1000);
 
     $('.price-filter__select').selectbox();
+
+    $('.product__rev-lnk').click(function () {
+        var selected = $(this).attr('href');
+        $.scrollTo(selected, 1000, { offset: -70 });
+    });
+    $('.product__more').click(function () {
+        var selected = $(this).attr('href');
+        $.scrollTo(selected, 1000, { offset: -70 });
+    });
+
+    $('.fancy').fancybox();
+
+    $('.product__foto-item a').click(function () {
+       var pic = $(this).attr('data-src');
+
+       $('.product__pic--big img').attr('src',pic);
+        $('.product__pic--big a').attr('href',pic);
+    });
+
 });
